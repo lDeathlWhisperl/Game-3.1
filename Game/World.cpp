9097d177fb3d356a03f726164c50e5d7e5717954
Илья Митин <iliya2001@mail.ru>
2022-10-02 18:14:28 +0300

    save

--- Game/World.cpp
+++ Game/World.cpp
@@ -64,25 +64,34 @@ void World::generator()
 
 	for (int y = 0; y < width; y++)
 		for (int x = 0; x < length; x++)
-			polygons[y][x] += cos(25 * polygons[y][x] * 180 / 3.14) * 25;
+			polygons[y][x] += cos(25 * polygons[y][x] * 180 / 3.14) + cos(25 * polygons[y][x] * 180 / 3.14) * 25;
 
 	smooth();
 
 	for (int y = 0; y < width; y++)
 		for (int x = 0; x < length; x++)
-			polygons[y][x] += 1;
+			polygons[y][x] += -1;
 
 	smooth();
 
 	for (int y = 0; y < width; y++)
 		for (int x = 0; x < length; x++)
-			if (polygons[y][x] > 0)
+			if (polygons[y][x] > -4)
 				polygons[y][x] = 0;
 
 	for (int y = 1; y < width - 1; y++)
 		for (int x = 1; x < length - 1; x++)
 			if (polygons[y][x] == 0 && random(0, 10) <= 1)
 				plantTree(x, y);
+
+	draw();
+	system("pause");
+	system("cls");
+
+	for (int y = 1; y < width - 1; y++)
+		for (int x = 1; x < length - 1; x++)
+			if (polygons[y][x] == -8 && polygons[y - 1][x] >= -7 && polygons[y][x - 1] >= -7 && polygons[y][x + 1] >= -7 && polygons[y + 1][x] >= -7)
+				polygons[y][x] = 0;
 }
 
 std::string World::paint(int high)
@@ -91,15 +100,17 @@ std::string World::paint(int high)
 
 	switch (high)
 	{
+	case -11:
+	case -10:
 	case -9:
-	case -8:
-	case -7:
 		res = "\x1b[34m";
 		break;
-	case -6:
-	case -5:
+	case -8:
 		res = "\x1b[94m";
 		break;
+	case -7:
+	case -6:
+	case -5:
 	case -4:
 	case -3:
 	case -2:
