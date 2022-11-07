09d83ba7606148d8e7634221dfbfbb6619034a47
Илья Митин <iliya2001@mail.ru>
2022-11-08 00:35:08 +0300

    Castle

--- Game/Castle.h
+++ Game/Castle.h
@@ -3,28 +3,81 @@
 class Castle
 {
 public:
-	void draw(double **world, int x, int y)
+	void draw(double** world, int x, int y)
 	{
 		int t_x = x,
 			t_y = y;
 
-		x -= 15;
+		int door = 776,
+			stone = 777,
+			window = 775;
+		//
+		x -= 14;
 		if (x < 0) x = 0;
 
-		for (int i = x; i < t_x; i++)
-			world[y][i] = 777;
+		for (int i = x; i <= t_x; i++)
+			world[y][i] = stone;
 
+		if (t_x - 7 >= 0)
+			world[t_y][t_x - 7] = door;
+
+		if (y == 0) return;
+		//
 		y--;
 		if (y < 0) y = 0;
 
-		for (int i = x, count = 1; i < t_x; i++, count++)
-			if (count == 2 || count == 6 || count == 10 || count == 14)
-				world[y][i] = 775;
+		for (int i = t_x, count = 3; i >= x; i--, count++)
+			if (count % 4 == 0)
+				world[y][i] = window;
 			else
-				world[y][i] = 777;
+				world[y][i] = stone;
 
-		if (t_x - 8 >= 0)
-			world[t_y][t_x - 8] = 776;
+		if (y == 0) return;
+		//
+		y--;
+		if (y < 0) y = 0;
 
+		for (int i = x; i <= t_x; i++)
+			world[y][i] = stone;
+
+		if (y == 0) return;
+		//
+		y--;
+		if (y < 0) y = 0;
+
+		for (int i = t_x, count = 1; i >= x; count++, i--)
+			if (((count % 2 != 0 && count < 5) || (count % 2 != 0 && count > 11)) || (count > 4 && count < 12))
+				world[y][i] = stone;
+
+		if (y == 0) return;
+		//
+		y--;
+		if (y < 0) y = 0;
+
+		for (int i = t_x, count = 1; i >= x; count++, i--)
+			if (count == 12)
+				break;
+			else if (count == 6 || count == 10)
+				world[y][i] = window;
+			else if (count > 4)
+				world[y][i] = stone;
+
+		if (y == 0) return;
+		//
+		y--;
+		if (y < 0) y = 0;
+
+		for (int i = t_x, count = 1; i >= x; count++, i--)
+			if (count > 4 && count < 12)
+				world[y][i] = stone;
+
+		if (y == 0) return;
+		//
+		y--;
+		if (y < 0) y = 0;
+
+		for (int i = t_x, count = 1; i >= x; count++, i--)
+			if (count > 4 && count < 12 && count % 2 != 0)
+				world[y][i] = stone;
 	}
 };
--- Game/World.cpp
+++ Game/World.cpp
@@ -67,16 +67,16 @@ void World::generator(unsigned int seed, int x_index, int y_index)
 					map[y][x] = 3;
 
 			//foliage noise
-			//if ((map[y][x] > 0.34 && map[y][x] <= 0.5 || map[y][x] >= 0.546) && map[y][x] < 1 && y > 0 && x > 0)
-			//	if (pn.noise(222 * t_x, 222 * t_y, 600) >= 0.38 && pn.noise(222 * t_x, 222 * t_y, 600) <= 0.45)
-			//		plantTree(x, y);
+			if ((map[y][x] > 0.34 && map[y][x] <= 0.5 || map[y][x] >= 0.546) && map[y][x] < 1 && y > 0 && x > 0)
+				if (pn.noise(222 * t_x, 222 * t_y, 600) >= 0.38 && pn.noise(222 * t_x, 222 * t_y, 600) <= 0.45)
+					plantTree(x, y);
 
 			if (map[y][x] == 3 && x > 0 && x < length - 1 && y > 0 && y < width - 1)
 				if (pn.noise(555 * t_x, 555 * t_y, 600) >= 0.38 && pn.noise(555 * t_x, 555 * t_y, 600) <= 0.45)
 					plantCactus(x, y);
 			
-			//spawn castle 8.88
-			if (pn.noise(555 * t_x, 555 * t_y, 600) * 10 >= 8.5 && map[y][x] > 0.3 && map[y][x] != 3 && (map[y][x] < 0.5 || map[y][x] > 0.545) && x < length)
+			//spawn castle
+			if (pn.noise(555 * t_x, 555 * t_y, 600) * 10 >= 8.88 && map[y][x] > 0.3 && map[y][x] != 3 && (map[y][x] < 0.5 || map[y][x] > 0.545) && x < length)
 				castle.draw(map, x, y);
 		}
 }
